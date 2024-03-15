23ba113a29a192727963a806360c11ff54a41986
Michael <michael@Zipcoders-MacBook-Pro-4.local>
2024-03-14 21:31:48 -0400

    Updated Dice Class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,32 @@
+import java.util.Random;
+
 public class Dice {
 
+    /*
+    Create Instant Variables
+    -=-=-=-=-=-=-=-=-=-=-=-=-
+    Int Number of Rolls.
+    Random for random number.
+    -=-=-=-=-=-=-=-=-=-=-=-=-
+     */
+    private int numRolls;
+    private Random random;
+
+
+    // Create Constructor for Dice
+    public Dice(int numRolls){
+        this.numRolls = numRolls;
+        this.random = new Random();
+    }
+
+    // Create method for Toss and Sum.
+    private Integer tossAndSum() {
+        int sum = 0;
+
+        for (int i = 0; i < this.numRolls; i++) {
+            sum += random.nextInt(6) + 1;   // Gets random number from dice.
+        }
+        return sum;
+    }
 
 }
